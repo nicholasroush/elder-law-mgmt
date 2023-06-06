dc92c7f6d06e811e1af3e3906726db74993f5839
ImCalicoJack <roush757@gmail.com>
2023-06-06 11:50:22 -0400

    new push for portfolio link

--- src/App.js
+++ src/App.js
@@ -1,39 +1,63 @@
-import './App.css';
-import { Nav } from './components/nav/nav';
-import { Home } from './components/containers/home/home';
-import { Services } from './components/containers/services/services';
-import { Workshops } from './components/containers/workshops/workshops';
-import { Team } from './components/containers/team/team';
-import { Resources } from './components/containers/resources/resources';
-import { Contact } from './components/containers/contacts/contact';
-import { Privacy } from './components/nav/footerLinks/privacy';
-import { Disclaimer } from './components/nav/footerLinks/disclaimer';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-
+import "./App.css";
+import { Nav } from "./components/nav/nav";
+import { Home } from "./components/containers/home/home";
+import { Services } from "./components/containers/services/services";
+import { Workshops } from "./components/containers/workshops/workshops";
+import { Team } from "./components/containers/team/team";
+import { Resources } from "./components/containers/resources/resources";
+import { Contact } from "./components/containers/contacts/contact";
+import { Privacy } from "./components/nav/footerLinks/privacy";
+import { Disclaimer } from "./components/nav/footerLinks/disclaimer";
+import { HashRouter, Switch, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Nav />
-        <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/services'  component={Services} />
-          <Route path='/workshops'  component={Workshops} />
-          <Route path='/team'  component={Team} />
-          <Route path='/resources'  component={Resources} />
-          <Route path='/contact'  component={Contact} />
-          <Route path='/privacy'  component={Privacy} />
-          <Route path='/disclaimer'  component={Disclaimer} />
-        </Switch>
-      </BrowserRouter>
-      <div className='footer'>
-        <a href='/privacy' target='_blank' rel='noreferrer'>Privacy Policy</a> | <a href='/disclaimer' target='_blank' rel='noreferrer'>Disclaimer</a> | <a href='https://reports.adviserinfo.sec.gov/reports/ADV/294012/PDF/294012.pdf' target='_blank' rel="noreferrer">ADV Form</a> | <a href='/contact'>Contact Us</a>
-        <p className='footer-disclaimer'>The information you obtain at this site is not, nor is it intended to be, legal advice. You should consult an attorney for advice regarding your individual situation. We invite you to contact us and welcome your calls, letters and electronic mail. Contacting us does not create an attorney-client relationship. Please do not send any confidential information to us until such time as an attorney-client relationship has been established.</p>
-        <p className='copy-right'>© 2021 by Elder Law Management. All rights reserved.</p>
-      </div>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<HashRouter>
+				<Nav />
+				<Switch>
+					<Route path='/' exact component={Home} />
+					<Route path='/services' component={Services} />
+					<Route path='/workshops' component={Workshops} />
+					<Route path='/team' component={Team} />
+					<Route path='/resources' component={Resources} />
+					<Route path='/contact' component={Contact} />
+					<Route path='/privacy' component={Privacy} />
+					<Route path='/disclaimer' component={Disclaimer} />
+				</Switch>
+			</HashRouter>
+			<div className='footer'>
+				<a href='/privacy' target='_blank' rel='noreferrer'>
+					Privacy Policy
+				</a>{" "}
+				|{" "}
+				<a href='/disclaimer' target='_blank' rel='noreferrer'>
+					Disclaimer
+				</a>{" "}
+				|{" "}
+				<a
+					href='https://reports.adviserinfo.sec.gov/reports/ADV/294012/PDF/294012.pdf'
+					target='_blank'
+					rel='noreferrer'
+				>
+					ADV Form
+				</a>{" "}
+				| <a href='/contact'>Contact Us</a>
+				<p className='footer-disclaimer'>
+					The information you obtain at this site is not, nor is it intended to
+					be, legal advice. You should consult an attorney for advice regarding
+					your individual situation. We invite you to contact us and welcome
+					your calls, letters and electronic mail. Contacting us does not create
+					an attorney-client relationship. Please do not send any confidential
+					information to us until such time as an attorney-client relationship
+					has been established.
+				</p>
+				<p className='copy-right'>
+					© 2021 by Elder Law Management. All rights reserved.
+				</p>
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/components/nav/footerLinks/privacy.js
+++ src/components/nav/footerLinks/privacy.js
@@ -5,19 +5,21 @@ export function Privacy() {
 
     return (
         <div className='privacy-main'>
-            <h1>Privacy Policy</h1>
-            <p>
-                ELM CAPITAL MANAGEMENT, LLC, a registered investment advisor, considers client privacy to  be a fundamental aspect of our client relationships. We are committed to  maintaining the confidentiality, integrity and security of our current, prospective, and former clients’ personal information.
-            </p> 
-            <p>
-                In the course of providing you with advisory services, we may collect, retain and  use client information for the purpose of administering our operations, providing  client service and complying with legal and regulatory compliance. This  information may come from sources such as account applications, investment  policy statements, from your transactions and other forms from other written,  electronic or verbal correspondence from your brokerage, attorney, accountant  or other advisor you may employ. We do not sell, exchange or disclose client  information with outside organizations unless the third party is essential in  administering our operations or except as required by law.
-            </p>
-            <p>
-                No confidential information, whatever the source, regarding any customer or  client, may be disclosed except the following: We reserve the right to disclose or  report personal information where we believe in good faith that disclosure is  required by law, to cooperate with regulators or law enforcement authorities.  Adviser reserves the right to disclose information with other adviser employees in  connection with the Adviser’s business and the non-affiliated third parties with  whom the Adviser has a contractual agreement to jointly offer, endorse or  sponsor a financial product or service; and to service and maintain customer  accounts including effectuating a transaction. Adviser may disclose information  about client or client’s account to a non-affiliated third party at client’s request.
-            </p>
-            <p>
-                Finally, to further safeguard client information digitally, we maintain password  protected systems, updated anti-virus and anti-spyware software, and encrypted  hardware and software firewalls.
-            </p>
+            <div className="privacy-content">
+                <h1>Privacy Policy</h1>
+                <p>
+                    ELM CAPITAL MANAGEMENT, LLC, a registered investment advisor, considers client privacy to  be a fundamental aspect of our client relationships. We are committed to  maintaining the confidentiality, integrity and security of our current, prospective, and former clients’ personal information.
+                </p> 
+                <p>
+                    In the course of providing you with advisory services, we may collect, retain and  use client information for the purpose of administering our operations, providing  client service and complying with legal and regulatory compliance. This  information may come from sources such as account applications, investment  policy statements, from your transactions and other forms from other written,  electronic or verbal correspondence from your brokerage, attorney, accountant  or other advisor you may employ. We do not sell, exchange or disclose client  information with outside organizations unless the third party is essential in  administering our operations or except as required by law.
+                </p>
+                <p>
+                    No confidential information, whatever the source, regarding any customer or  client, may be disclosed except the following: We reserve the right to disclose or  report personal information where we believe in good faith that disclosure is  required by law, to cooperate with regulators or law enforcement authorities.  Adviser reserves the right to disclose information with other adviser employees in  connection with the Adviser’s business and the non-affiliated third parties with  whom the Adviser has a contractual agreement to jointly offer, endorse or  sponsor a financial product or service; and to service and maintain customer  accounts including effectuating a transaction. Adviser may disclose information  about client or client’s account to a non-affiliated third party at client’s request.
+                </p>
+                <p>
+                    Finally, to further safeguard client information digitally, we maintain password  protected systems, updated anti-virus and anti-spyware software, and encrypted  hardware and software firewalls.
+                </p>
+            </div>
         </div>
     )
 }
